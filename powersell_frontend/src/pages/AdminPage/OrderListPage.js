9e20f10a39c5441a52695e9a27a7aca98dace58b
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-08-30 03:41:10 +0900

    admin category

--- powersell_frontend/src/pages/AdminPage/OrderListPage.js
+++ powersell_frontend/src/pages/AdminPage/OrderListPage.js
@@ -10,28 +10,68 @@ import spinner from "../../images/icons/spinner.gif";
 
 const OrderListPage = (props) => {
   const [result, setResult] = useState([]);
-  const [selected, setSelected] = useState("배송전체");
-  const [productSelected, setProductSelected]= useState("상품전체");
   const [isLoading, setIsLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const [searchCount, setSearchCount] = useState("");
   const [searchName, setSearchName] = useState("");
   const [searchNumber, setSearchNumber] = useState("");
-  const selectDict = {
-    배송전체: "",
+
+  // 출시 회차
+  const [testOrderSelected, setTestOrderSelected] = useState("1회차");
+  const testOrderDict = {
+    // "2회차": "2회차",
+    "1회차": "1회차",
+    전체: "전체",
+  };
+  const testOrderSelect = (e) => {
+    setTestOrderSelected(e.target.value);
+  };
+
+  // 배송 상태
+  const [statusSelected, setStatusSelected] = useState("배송상태");
+  const statusDict = {
+    배송상태: "",
     입금대기: "WAITING",
     배송중: "DELIVERING",
     배송완료: "ARRIVED",
     주문취소: "CANCELED",
   };
+  const statusSelect = (e) => {
+    setStatusSelected(e.target.value);
+  };
+
+  // 상품명
+  const [productSelected, setProductSelected] = useState("상품명");
   const productDict = {
-    상품전체: "",
+    상품명: "",
     삼다수: "제주 삼다수 2L (6개입)",
     햇반: "햇반 백미밥 210g (3개입)",
     컵밥: "오뚜기 컵밥 오삼불고기덮밥 310g",
     라면: "농심 신라면 (5개입)",
     구운란: "곰곰 구운란 10구",
   };
+  const productSelect = (e) => {
+    setProductSelected(e.target.value);
+  };
+
+  // 주문 개수
+  const [countSelected, setCountSelected] = useState("주문개수");
+  const countDict = {
+    주문개수: "",
+    "1개": 1,
+    "2개": 2,
+  };
+  const countSelect = (e) => {
+    setCountSelected(e.target.value);
+  };
+
+  // search function
+  const onChangeName = (e) => {
+    setSearchName(e.target.value);
+  };
+  const onChangeNumber = (e) => {
+    setSearchNumber(e.target.value);
+  };
+
+  // filtered result
   const filteredResult = [...result]
     .sort((a, b) => {
       if (a.orderDate > b.orderDate) return -1;
@@ -39,48 +79,59 @@ const OrderListPage = (props) => {
       return 0;
     })
     .filter((order) => {
-      const page1 = document.getElementById("page1");
-      const page2 = document.getElementById("page2");
-      if (page === 2) {
-        page1.style.backgroundColor = "white";
-        page2.style.backgroundColor = "#fa7979";
-        return order.orderDate >= "2023-09-04 21:00:00" && order.orderDate <= "2023-09-10 23:59:59";
+      if (testOrderDict[testOrderSelected] === "2회차") {
+        return (
+          order.orderDate >= "2023-09-04 21:00:00" &&
+          order.orderDate <= "2023-09-10 23:59:59"
+        );
+      }
+      else if (testOrderDict[testOrderSelected] === "1회차") {
+        return (
+          order.orderDate >= "2023-08-28 21:00:00" &&
+          order.orderDate <= "2023-09-03 23:59:59"
+        );
       }
       else {
-        page1.style.backgroundColor = "#fa7979";
-        page2.style.backgroundColor = "white";
-        return order.orderDate >= "2023-08-28 21:00:00" && order.orderDate <= "2023-09-03 23:59:59";
-      }
-    })
-    .filter((order) => {
-      if (selected === "배송전체") {
         return true;
       }
-      return order.orderStatus === selectDict[selected];
     })
     .filter((order) => {
-      if (productSelected === "상품전체") {
+      if (statusSelected === "배송상태") {
+        return true;
+      }
+      return order.orderStatus === statusDict[statusSelected];
+    })
+    .filter((order) => {
+      if (productSelected === "상품명") {
         return true;
       }
       return order.item.name === productDict[productSelected];
     })
     .filter((order) => {
-      if (searchCount === "") {
+      if (countSelected === "주문개수") {
         return true;
       }
-      return order.count.toString().replace(" ","").toLocaleLowerCase().includes(searchCount.toLocaleLowerCase().replace(" ",""));
+      return order.count === countDict[countSelected];
     })
     .filter((order) => {
       if (searchName === "") {
         return true;
       }
-      return order.name.toString().replace(" ","").toLocaleLowerCase().includes(searchName.toLocaleLowerCase().replace(" ",""));
+      return order.name
+        .toString()
+        .replace(" ", "")
+        .toLocaleLowerCase()
+        .includes(searchName.toLocaleLowerCase().replace(" ", ""));
     })
     .filter((order) => {
       if (searchNumber === "") {
         return true;
       }
-      return order.number.toString().replace(" ","").toLocaleLowerCase().includes(searchNumber.toLocaleLowerCase().replace(" ",""));
+      return order.number
+        .toString()
+        .replace(" ", "")
+        .toLocaleLowerCase()
+        .includes(searchNumber.toLocaleLowerCase().replace(" ", ""));
     });
 
   // product count
@@ -88,25 +139,6 @@ const OrderListPage = (props) => {
   for (let i = 0; i < filteredResult.length; i++) {
     productCount += filteredResult[i].count;
   }
-
-  // function: select standard
-  const handleSelect = (e) => {
-    setSelected(e.target.value);
-  };
-  const productSelect = (e) => {
-    setProductSelected(e.target.value);
-  };
-
-  // search function
-  const onChangeCount = (e) => {
-    setSearchCount(e.target.value);
-  };
-  const onChangeName = (e) => {
-    setSearchName(e.target.value);
-  };
-  const onChangeNumber = (e) => {
-    setSearchNumber(e.target.value);
-  };
 
   // REST API 1-1
   const [reloadFlag, setReloadFlag] = useState(0);
@@ -126,38 +158,47 @@ const OrderListPage = (props) => {
 
   return (
     <div className={styles.order_main}>
-      {/* test order */}
-      <div className={styles.testOrder}>
-        <div id="page1" onClick={() => setPage(1)}><div>1</div></div>
-        <div id="page2" onClick={() => setPage(2)}><div>2</div></div>
+      {/* first sort selectBox */}
+      <div className={styles.firstSelectBox}>
+        <select onChange={testOrderSelect} value={testOrderSelected}>
+          {Object.keys(testOrderDict).map((testOrder) => (
+            <option value={testOrder} key={testOrder}>
+              {testOrder}
+            </option>
+          ))}
+        </select>
+        <select onChange={statusSelect} value={statusSelected}>
+          {Object.keys(statusDict).map((status) => (
+            <option value={status} key={status}>
+              {status}
+            </option>
+          ))}
+        </select>
+        <select onChange={productSelect} value={productSelected}>
+          {Object.keys(productDict).map((product) => (
+            <option value={product} key={product}>
+              {product}
+            </option>
+          ))}
+        </select>
       </div>
 
-      {/* sort selectBox */}
-      <div className={styles.selectBox}>
-        <div>
-          <select onChange={handleSelect} value={selected}>
-            {Object.keys(selectDict).map((status) => (
-              <option value={status} key={status}>
-                {status}
-              </option>
-            ))}
-          </select>
-          <select onChange={productSelect} value={productSelected}>
-            {Object.keys(productDict).map((product) => (
-              <option value={product} key={product}>
-                {product}
-              </option>
-            ))}
-          </select>
-        </div>
-        <span>주문/상품: {filteredResult.length}/{productCount}</span>
+      {/* second sort selectBox */}
+      <div className={styles.secondSelectBox}>
+        <select onChange={countSelect} value={countSelected}>
+          <option value="주문개수">주문개수</option>
+          <option value="1개">1개</option>
+          <option value="2개">2개</option>
+        </select>
+        <input type="text" placeholder="주문자명" onChange={onChangeName} />
+        <input type="text" placeholder="전화번호" onChange={onChangeNumber} />
       </div>
 
-      {/* search bar */}
-      <div className={styles.searchBar}>
-        <input type="text" placeholder="주문개수" onChange={onChangeCount}/>
-        <input type="text" placeholder="주문자명" onChange={onChangeName}/>
-        <input type="text" placeholder="전화번호" onChange={onChangeNumber}/>
+      {/* order count */}
+      <div className={styles.orderCount}>
+        <span>
+          주문: {filteredResult.length}개 / 상품: {productCount}개
+        </span>
       </div>
 
       {/* total order list */}
@@ -212,7 +253,8 @@ const OrderListPage = (props) => {
                       {order.orderDate.replace("T", " ").slice(0, 19)}
                     </div>
                     <div>
-                      <span>주소</span>: ({order.zipcode}) {order.address} {order.dongho}
+                      <span>주소</span>: ({order.zipcode}) {order.address}{" "}
+                      {order.dongho}
                     </div>
                   </div>
                 </div>
