a67fa9efe01f1b59706880cb1139e513cff9d9c4
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-20 22:12:20 +0900

    API URL 수정

--- powersell_frontend/src/hostURL.js
+++ powersell_frontend/src/hostURL.js
@@ -1,4 +1,4 @@
-const hostURL = "https://be.cheapat9.com";
+const hostURL = "https://cheapat9-be-service-5e6zpasnkq-an.a.run.app";
 // const hostURL = "http://localhost:8080"
 
 export default hostURL;
